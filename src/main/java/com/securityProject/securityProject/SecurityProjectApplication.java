38e724b3c4c7b1227de118699e429979e402031f
tharikathushani <tharikathushani212@gmail.com>
2026-03-02 17:23:04 +0530

    main and others

---
+++ src/main/java/com/securityProject/securityProject/SecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.securityProject.securityProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProjectApplication.class, args);
+	}
+
+}
